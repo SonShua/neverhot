bfc4415de1f31cc2fb52fe5bfa20e144fd2a5ca0
SonShua <77282001+SonShua@users.noreply.github.com>
2023-06-21 14:20:19 +0200

    Started pages and weather app

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "users",
+    "weather",
+    "pages",
     # 3rd party
     "crispy_forms",
     "crispy_bootstrap5",
