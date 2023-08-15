63243d9002cf75284dd327907c19e5b644046937
SonShua <77282001+SonShua@users.noreply.github.com>
2023-08-15 13:17:37 +0200

    updated tests for weather

--- weather/tests.py
+++ weather/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from .models import City, Forecast
 from background_task import background
 from background_task.tasks import tasks
@@ -23,27 +24,26 @@ class CityTestCase(TestCase):
 class ForecastTestCase(TestCase):
     def setUp(self):
         # This is the only method used
-        City.objects.create(city_name="Berlin", lat=52.5170365, lon=13.3888599)
-        get_weather_forecast("Berlin")
+        berlin = City.objects.create(city_name="Berlin", lat=52.5170365, lon=13.3888599)
+        get_weather_forecast(berlin.pk)
         # City.objects.create(city_name="Berlin", lat=52.5170365, lon=13.3888599)
         return super().setUp()
 
     def test_forecast_has_values(self):
         all_forecasts = Forecast.objects.all()
         # Should be 10 elements created and every field should be filled
-        for x in range(0, 10):
-            self.assertEqual(str(all_forecasts[x].city), "Berlin")
-            self.assertGreater(all_forecasts[x].temp, -20)
-            self.assertGreater(all_forecasts[x].temp_feel, -20)
-            self.assertTrue(0 <= all_forecasts[x].hum <= 100)
-            self.assertTrue(all_forecasts[x].wind_speed)
-            self.assertTrue(all_forecasts[x].icon)
+        for forecast in all_forecasts:
+            self.assertEqual(str(forecast.city), "Berlin")
+            self.assertGreater(forecast.temp, -20)
+            self.assertGreater(forecast.temp_feel, -20)
+            self.assertTrue(0 <= forecast.hum <= 100)
+            self.assertTrue(forecast.wind_speed)
+            self.assertTrue(forecast.icon)
 
     def test_url_exists_at_correct_location(self):
         berlin = City.objects.get(city_name="Berlin")
-        response = self.client.get(f"/weather/{berlin.pk}")
+        response = self.client.get(reverse("city_detail", kwargs={"pk": berlin.pk}))
         self.assertEqual(response.status_code, 200)
-        # Add testing for the chartjs object
 
 
 # Background db update tasks test
