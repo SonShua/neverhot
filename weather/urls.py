e3e4b4fa734a77f465b9087b2ff2b98b34d282e3
SonShua <77282001+SonShua@users.noreply.github.com>
2023-08-19 12:18:31 +0200

    Finished the table

--- weather/urls.py
+++ weather/urls.py
@@ -10,7 +10,7 @@ from .views import (
 urlpatterns = [
     path("", CityView.as_view(), name="weather"),
     path("<int:pk>/", CityDetailView.as_view(), name="city_detail"),
-    path("table/", CityHTMxTableView.as_view(), name="table")
+    path("search/", CityHTMxTableView.as_view(), name="search")
     # path("new/", CityAddView, name="city_new"),
     # path("city_select/", CitySelectView, name="city_select"),
     # path("no_city_found/", NoCityView, name="no_city_found"),
