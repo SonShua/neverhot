9d7f09c3e92d99d77bade845f3f43399f7f10007
SonShua <77282001+SonShua@users.noreply.github.com>
2023-08-23 12:31:27 +0200

    added database url

--- django_project/settings.py
+++ django_project/settings.py
@@ -102,10 +102,10 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgresql://postgres:postgres@localhost:5432/neverhot",
+        conn_max_age=600,
+    )
 }
 
 
