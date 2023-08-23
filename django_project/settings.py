53c9c0aa77ee9fb444f2b5cf65c11e32b575ad35
SonShua <77282001+SonShua@users.noreply.github.com>
2023-08-23 18:07:40 +0200

    changed the whitenoise static storage to work with render

--- django_project/settings.py
+++ django_project/settings.py
@@ -174,14 +174,14 @@ PARLER_LANGUAGES = {
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:  # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
