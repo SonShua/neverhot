bc002107b264919e90218cdd58d77c6a9535d73f
SonShua <77282001+SonShua@users.noreply.github.com>
2023-08-25 10:33:06 +0200

    updated settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -35,10 +35,11 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-DEBUG_PROPAGATE_EXCEPTIONS = True
+DEBUG = True
+if DEBUG:
+    DEBUG_PROPAGATE_EXCEPTIONS = True
 
-ALLOWED_HOSTS = ["www.neverhot.org"]
+ALLOWED_HOSTS = [""]
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
